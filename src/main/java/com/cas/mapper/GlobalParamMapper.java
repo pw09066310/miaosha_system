5b6a3d2dbd6a302447b42a352c1432ee42a31d20
pengwei@cx-gyl.com <pengwei@cx-gyl.com>
2020-09-24 17:10:34 +0800

    提交redis util

---
+++ src/main/java/com/cas/mapper/GlobalParamMapper.java
@@ -0,0 +1,14 @@
+package com.cas.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.cas.bean.GlobalParam;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @ClassName : GlobalParamMapper
+ * @Description :
+ * @Author : pw
+ * @Date: 2020-09-24 14:22
+ */
+public interface GlobalParamMapper extends BaseMapper<GlobalParam> {
+}
